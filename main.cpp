7dd49944c406a9a1e85d96dc182c1708cb006ef2
Hassan Rasmy <88991223+TeddyBearies@users.noreply.github.com>
2026-02-27 17:52:54 +0000

    Parse command line parameters

--- main.cpp
+++ main.cpp
@@ -5,6 +5,13 @@
 
 using namespace std;
 
+static void printUsage(const char* prog) {
+    cout << "usage:\n";
+    cout << "  " << prog << " <trace_file> <max_frames> <tick_every> <bits> <output_csv>\n";
+    cout << "example:\n";
+    cout << "  " << prog << " trace.txt 50 10 8 results.csv\n";
+}
+
 static vector<int> readTraceFile(const string& path) {
     vector<int> trace;
     ifstream in(path);
@@ -15,9 +22,7 @@ static vector<int> readTraceFile(const string& path) {
     }
 
     int x;
-    while(in >> x) {
-        trace.push_back(x);
-    }
+    while(in >> x) trace.push_back(x);
 
     return trace;
 }
@@ -25,27 +30,41 @@ static vector<int> readTraceFile(const string& path) {
 int main(int argc , char* argv[]) {
     cout << "paging aging simulator (task 3)\n";
 
-    if(argc < 2) {
-        cout << "usage: " << argv[0] << " <trace_file>\n";
+    if(argc < 6) {
+        printUsage(argv[0]);
         return 0;
     }
 
-    string filePath = argv[1];
-    vector<int> trace = readTraceFile(filePath);
+    string tracePath = argv[1];
+    int maxFrames = 0;
+    int tickEvery = 0;
+    int bits = 0;
+    string outCsv = argv[5];
 
+    try {
+        maxFrames = stoi(argv[2]);
+        tickEvery = stoi(argv[3]);
+        bits = stoi(argv[4]);
+    } catch(...) {
+        cout << "bad number in args\n";
+        printUsage(argv[0]);
+        return 0;
+    }
+
+    if(maxFrames <= 0 || tickEvery <= 0 || bits <= 0 || bits > 32) {
+        cout << "args not valid (max_frames>0, tick_every>0, bits 1..32)\n";
+        return 0;
+    }
+
+    vector<int> trace = readTraceFile(tracePath);
     if(trace.empty()) {
         cout << "trace is empty (or file format not plain integers)\n";
         return 0;
     }
 
-    cout << "trace file: " << filePath << "\n";
-    cout << "refs read: " << trace.size() << "\n";
-
-    cout << "first few: ";
-    for(int i = 0; i < (int)trace.size() && i < 10; i++) {
-        cout << trace[i] << " ";
-    }
-    cout << "\n";
+    cout << "trace: " << tracePath << "  refs=" << trace.size() << "\n";
+    cout << "maxFrames=" << maxFrames << " tickEvery=" << tickEvery << " bits=" << bits << "\n";
+    cout << "csv output will be: " << outCsv << "\n";
 
     return 0;
 }
